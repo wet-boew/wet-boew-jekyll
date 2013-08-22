01a636412e3cb8e6aefb7b91499c3b9154acdd5d
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-08-22 11:42:54 -0400

    Fixed edit page link id.

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -3,7 +3,7 @@
 	"use strict";
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
-		var edit_link = document.getElementById('edit-link');
+		var edit_link = document.getElementById('edit-page');
 		if (edit_link !== null) {
 			edit_link.href = edit_link.href.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/');
 		}
